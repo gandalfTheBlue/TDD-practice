215d8eba584ff82d85b6d2e0895168f501c41ded
andouillette-ys <yangshan0823@outlook.com>
2019-08-27 22:15:21 +0800

    refactor two sum

--- src/twoSum/index.js
+++ src/twoSum/index.js
@@ -4,28 +4,15 @@
  * @return {number[]}
  */
 module.exports = function twoSum(nums, target) {
-  if (!nums || !Array.isArray(nums) || nums.length < 2) {
-    return null
-  }
-
   let obj = {}
-  let result = null
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i]
-    if (obj[target - num]) {
-      result = [num, obj[target - num]]
-      break
+    if (obj[target - num] >= 0) {
+      return [obj[target - num], i]
     } else {
-      if (!obj[num]) {
-        obj[num] = num
-      } else {
-        if (num + num === target) {
-          result = [num, num]
-          break
-        }
-      }
+      obj[num] = i
     }
   }
 
-  return result
+  return null
 }
--- src/twoSum/index.test.js
+++ src/twoSum/index.test.js
@@ -1,27 +1,11 @@
 const twoSum = require('./index')
 
-it('should return null if passed invalid parameters', () => {
-  const invalidParam1 = null
-  const invalidParam2 = [1]
-
-  expect(twoSum(invalidParam1, 5)).toBe(null)
-  expect(twoSum(invalidParam2, 5)).toBe(null)
-})
-
-it('should return null if not meet the requirement', () => {
-  const param = [1, 7]
-
-  const result = twoSum(param, 6)
-
-  expect(result).toBe(null)
-})
-
 it('should return the same array if meet the requirement', () => {
   const param = [2, 9]
 
   const result = twoSum(param, 11)
 
-  expect(result[0] + result[1]).toBe(11)
+  expect(result).toStrictEqual([0, 1])
 })
 
 it('should return the same array if meet the requirement', () => {
@@ -29,7 +13,7 @@ it('should return the same array if meet the requirement', () => {
 
   const result = twoSum(param, 6)
 
-  expect(result[0] + result[1]).toBe(6)
+  expect(result).toStrictEqual([0, 3])
 })
 
 it('should return the same array if meet the requirement', () => {
@@ -37,13 +21,5 @@ it('should return the same array if meet the requirement', () => {
 
   const result = twoSum(param, 8)
 
-  expect(result[0] + result[1]).toBe(8)
-})
-
-it('should return the null if not meet the requirement', () => {
-  const param = [4, 7, 5]
-
-  const result = twoSum(param, 8)
-
-  expect(result).toBe(null)
+  expect(result).toStrictEqual([0, 1])
 })
